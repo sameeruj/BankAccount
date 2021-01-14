39841af754079b4bfd3be9cfbd786e2c5ff2610a
sameeruj <sameeruj@yahoo.com>
2021-01-14 21:56:56 +0530

    1

---
+++ src/com/codewithsameer/Main.java
@@ -0,0 +1,11 @@
+package com.codewithsameer;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    var account = new Account();
+	    account.deposit(10);
+	    account.withdraw(5);
+        System.out.println(account.getBalance());
+    }
+}
