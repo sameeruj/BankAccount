bea31053920ba4c722c8f45f70ea3377df79be83
sameeruj <sameeruj@yahoo.com>
2021-01-15 19:30:27 +0530

    4

--- src/com/codewithsameer/Main.java
+++ src/com/codewithsameer/Main.java
@@ -3,12 +3,14 @@ package com.codewithsameer;
 public class Main {
 
     public static void main(String[] args) {
+        // create an interface of type Account interface.. With interfaces we can build loosely coupled interfaces.
+        // demonstrates encapsulation and data hiding
 	    var account = new Account();
 	    account.deposit(10);
 	    account.withdraw(5);
         System.out.println(account.getBalance());
-        System.out.println("EOF");
 
+        // demonstrates abstraction principle.
         var mailService = new MailService();
         mailService.sendEmail();
     }
