cba3b96d96e4759edfe03bca8bf904473dab1ee0
sameeruj <sameeruj@yahoo.com>
2021-01-15 21:37:12 +0530

    7

--- src/com/codewithsameer/Main.java
+++ src/com/codewithsameer/Main.java
@@ -10,17 +10,18 @@ public class Main {
 	    account.withdraw(5);
         System.out.println(account.getBalance());
 
-        // demonstrates abstraction principle.
+        // demonstrates abstraction principle. This means the end user do not need to know the hidden complexities.
         var mailService = new MailService();
         mailService.sendEmail();
 
-        // Inheritance demonstration
+        // Inheritance demonstration. Code reuse.
         var textBox = new TextBox();
         // you can see enabled gets printed textBox object is inheriting the functions defined in the base class UIControl.
         // Inheritance helps reuse of the code across classes without code duplication.
         textBox.enable();
 
-        //Polymorphism in action
+        //Polymorphism in action. Runtime polymorphism. Function taking different forms. draw() has different implementation in inherited classes.
+        //If you see below using the abstract class UIControl we are calling draw function of classes TextBox and Button.
         System.out.println("\n \nDrawing widgets");
         drawUIControl(new TextBox());
         drawUIControl(new Button());
