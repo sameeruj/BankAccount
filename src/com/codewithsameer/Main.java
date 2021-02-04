5940243c072220d2c8b7f86f7c9d3545476f3822
sameeruj <sameeruj@yahoo.com>
2021-02-04 12:37:11 +0530

    123

--- src/com/codewithsameer/Main.java
+++ src/com/codewithsameer/Main.java
@@ -1,5 +1,7 @@
 package com.codewithsameer;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -23,8 +25,16 @@ public class Main {
         //Polymorphism in action. Runtime polymorphism. Function taking different forms. draw() has different implementation in inherited classes.
         //If you see below using the abstract class UIControl we are calling draw function of classes TextBox and Button.
         System.out.println("\n \nDrawing widgets");
+        //Object can take different forms. It can behave as TextBox and Button.
         drawUIControl(new TextBox());
         drawUIControl(new Button());
+
+        //Jframe
+        JFrame frame = new JFrame();
+        frame.setTitle("Welcome Sameer!!!");
+        frame.setVisible(true);
+        frame.setSize(420,420);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 
